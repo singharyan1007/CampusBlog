a36a9a937836c781e12717c791accc4acc675bf0
singharyan1007 <77246158+singharyan1007@users.noreply.github.com>
2021-11-10 18:11:11 +0530

    deleted a few files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fileupload = require('express-fileupload');
 
+
 let initial_path = path.join(__dirname, "public");
 
 const app = express();
